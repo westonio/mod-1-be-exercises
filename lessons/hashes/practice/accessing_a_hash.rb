8eb90f6dd1e3e530c8229cfba600c64da3ba9504
Weston Schutt <wtschutt@gmail.com>
2023-05-19 10:47:48 -0600

    Adding hash exercises

--- lessons/hashes/practice/accessing_a_hash.rb
+++ lessons/hashes/practice/accessing_a_hash.rb
@@ -1,14 +1,14 @@
 suitcase = {
-  "socks" => 4,
-  "pants" => 1
+  socks: 4,
+  pants: 1
 }
 
 # How would we get the value of:
 
 # 1. "socks"
-suitcase["socks"]
+suitcase[:socks]
 # 2. "pants"
-suitcase["pants"]
+suitcase[:pants]
 # 3. All of the values in this hash.
 suitcase.values
 # 4. How would we get the keys of the above hash?
--- lessons/hashes/practice/adding_to_a_hash.rb
+++ lessons/hashes/practice/adding_to_a_hash.rb
@@ -1,15 +1,15 @@
 suitcase = {
-  "socks" => 4,
-  "pants" => 1
+  socks: 4,
+  pants: 1
 }
 
 # How can we add the following key/value pairs:
 # 1. jackets/2
-suitcase["jackets"] = 2
+suitcase[:jackets] = 2
 # 2. shirts/4
-suitcase["shirts"] = 4
+suitcase[:shirts] = 4
 # 3. shoes/2
-suitcase["shoes"] = 2
+suitcase[:shoes] = 2
 
 # How can we increase the existing value:
 # 1. Change pants to have a value of 2
